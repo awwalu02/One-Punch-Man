2a65e21e7a7b0561caa8874188e984ddc3a2b68b
awwalu <abdulkareemawwal13@gmail.com>
2025-07-01 20:48:33 +0100

    error change

--- game-on/src/app/error/error.tsx
+++ game-on/src/app/error/error.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-export default function back({error, reset}: {error: Error; reset: () => void;}){
+export default function ErrorPage({error, reset}: {error: Error; reset: () => void;}){
     return<>
     <h2><p>{error.message}</p></h2>
     <button onClick={() => reset()}>Resolve</button>
